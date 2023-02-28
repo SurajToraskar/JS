ab61d8505e07bd7ca913345b1d7294bd89649b6a
SurajToraskar <toraskarsurajsuryakant@gmail.com>
2023-02-28 12:49:38 +0530

    New Pattern

--- js-pattern-program.js
+++ js-pattern-program.js
@@ -7,5 +7,5 @@ for(let i=1;i<=rows;i+=1){
     for(let j=1;j<=columns;j+=1){
         document.getElementById("rect").innerHTML+=symbol;
     }
-     document.getElementById("rect").innerHTML+="<br/>"
+    document.getElementById("rect").innerHTML+="<br/>"
 }
