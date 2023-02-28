9c09f672e930e1dfb9a06c1ae5e3267bdf655bd3
SurajToraskar <toraskarsurajsuryakant@gmail.com>
2023-02-28 12:38:28 +0530

    Pattern

---
+++ js-pattern-program.js
@@ -0,0 +1,11 @@
+let rows=window.prompt("Enter The Rows:");
+let columns=window.prompt("Enter The Columns:");
+let symbol=window.prompt("Enter The Symbol:");
+
+
+for(let i=1;i<=rows;i+=1){
+    for(let j=1;j<=columns;j+=1){
+        document.getElementById("rect").innerHTML+=symbol;
+    }
+    document.getElementById("rect").innerHTML+="<br/>"
+}
